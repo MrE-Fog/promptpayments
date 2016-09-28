ce7a4fbcec2b8dab404557e6acd18a5aa5c14c2d
the-daniel-rothig <daniel.rothig@gmail.com>
2016-09-28 17:33:32 +0100

    Improved the weird routes and common.routes behaviour

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -7,8 +7,16 @@ import play.data.FormFactory;
 
 /**
  * Created by daniel.rothig on 27/09/2016.
+ *
+ * Main controller
  */
 public class Home extends Controller {
-    public Result index() {return ok(views.html.index.render()); }
+    public Result index() {return ok(views.html.Home.index.render()); }
 
+    public Result page(int page) {
+        switch (page) {
+            case 1: return ok(views.html.Home.page1.render());
+            default: return status(404);
+        }
+    }
 }
