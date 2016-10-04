f50f34eb56cd1fbc4fcc266b36fb259b489692ec
the-daniel-rothig <daniel.rothig@gmail.com>
2016-10-04 15:43:56 +0100

    Slightly less terrible schema

--- conf/db/migration/V1__CompanyAndReportTables.sql
+++ conf/db/migration/V1__CompanyAndReportTables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Company(
-    CompaniesHouseIdentifier nvarchar(30),
+    CompaniesHouseIdentifier nvarchar(30) PRIMARY KEY,
     Name nvarchar(256)
 );
 CREATE TABLE Report(
     Identifier SERIAL PRIMARY KEY,
-     CompaniesHouseIdentifier nvarchar(30) NOT NULL,
-     FilingDate date NOT NULL,
+     CompaniesHouseIdentifier nvarchar(30) NOT NULL REFERENCES Company(CompaniesHouseIdentifier),
+     FilingDate DATE NOT NULL,
      NumberOne DECIMAL DEFAULT 0.0,
      NumberTwo DECIMAL DEFAULT 0.0,
      NumberThree DECIMAL DEFAULT 0.0
