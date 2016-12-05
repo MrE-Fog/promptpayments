e2d02f166ac0e6c6099c434dfdc7ca0d2119ad40
the-daniel-rothig <daniel.rothig@gmail.com>
2016-12-05 16:57:28 +0000

    new info box; make info box styling more prominent

--- app/components/GovUkNotifyEmailer.java
+++ app/components/GovUkNotifyEmailer.java
@@ -1,11 +1,13 @@
 package components;
 
+import com.google.inject.ImplementedBy;
 import models.CompanySummary;
 import models.ReportSummary;
 
 /**
  * Created by daniel.rothig on 02/12/2016.
  */
+@ImplementedBy(GovUkNotifyEmailerImpl.class)
 public interface GovUkNotifyEmailer {
     boolean sendConfirmationEmail(String recipient, CompanySummary company, ReportSummary report, String url);
 }
